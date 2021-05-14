089a7e2de81f764482095fcea44a0ca456bcf9a4
Martin <bezet.martin@gmail.com>
2021-05-14 11:29:05 +0200

    Auto stash before merge of "master" and "origin/master"

--- leJeu/src/main/java/metier/Guerrier.java
+++ leJeu/src/main/java/metier/Guerrier.java
@@ -3,14 +3,14 @@ package metier;
 public class Guerrier extends Personnage
 {
 
-	
-	
-    public Guerrier(int id,int x, int y, int hP, int attaque, int intelligence, int pA, int pM) {
-        super(id,x, y, hP, attaque, intelligence, pA, pM);
+
+
+    public Guerrier(int id,int x, int y, int hP, int attaque, int intelligence, int pA, int pM, int regenPA, int maxPA) {
+        super(id,x, y, hP, attaque, intelligence, pA, pM, regenPA, maxPA);
     }
 
 
-    
+
     public Guerrier(int x, int y){
         super(x, y);
         this.hP = 100;
@@ -18,15 +18,17 @@ public class Guerrier extends Personnage
         this.intelligence = 5;
         this.pA = 8;
         this.pM = 3;
+        this.regenPA = 1 ;
+        this.maxPA = 10 ;
     }
 
 
 
-	@Override
-	public String toString() {
-		return "Guerrier [id=" + id + ", x=" + x + ", y=" + y + ", hP=" + hP + ", attaque=" + attaque
-				+ ", intelligence=" + intelligence + ", pA=" + pA + ", pM=" + pM + "]";
-	}
-    
-    
+    @Override
+    public String toString() {
+        return "Guerrier [hP=" + hP + ", attaque=" + attaque + ", intelligence=" + intelligence + ", pA=" + pA + ", pM="
+                + pM + ", regenPA=" + regenPA + "]";
+    }
+
+
 }
