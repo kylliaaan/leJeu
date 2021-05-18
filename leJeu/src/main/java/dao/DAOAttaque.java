a608cb84db7bc07ab0c17d668c16e3f257bd8dd0
Kyllian <kyllian.bourgade@gmail.com>
2021-05-18 12:20:41 +0200

    ++

--- leJeu/src/main/java/dao/DAOAttaque.java
+++ leJeu/src/main/java/dao/DAOAttaque.java
@@ -12,7 +12,7 @@ import util.Context;
 public class DAOAttaque implements IDAO<Attaque>{
 
 	@Override
-	public List<Attaque> findById(int id) {
+	public Attaque findById(int id) {
 		EntityManager em = Context.get_instance().getEmf().createEntityManager();
 
 		Attaque u=em.find(Attaque.class, id);
